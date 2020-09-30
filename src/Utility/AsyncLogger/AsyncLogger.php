31751987f5822d05d07924d1cfe3bbea3c9a9181
Volodymyr Polishchuk <volodymyrpo@softwareplanet.uk.com>
2020-09-30 11:00:50 +0300

    DFSAAS-138 Optimize DecisionDeskHQ environment to handle high traffic load
    - Add catching errors

--- src/Utility/AsyncLogger/AsyncLogger.php
+++ src/Utility/AsyncLogger/AsyncLogger.php
@@ -11,32 +11,39 @@ class AsyncLogger
 {
     protected static $FILE = 'async-request.log';
 
+    static function scheduleCallHandler() {
+        Log::info('AsyncLogger::scheduleCallHandler - Check logs for saving');
+        if (file_exists(storage_path(AsyncLogger::$FILE))) {
+            Log::info('AsyncLogger::scheduleCallHandler - Logs found. Saving logs to database');
+            /** @var string $content */
+            $content = file_get_contents(storage_path(AsyncLogger::$FILE));
+            unlink(storage_path(AsyncLogger::$FILE));
+            /** @var array $requests */
+            $requests = explode("\n", $content);
+            $arr = [];
+            foreach ($requests as $request) {
+                if (strlen($request) != 0) {
+                    try {
+                        $arr[] = unserialize($request);
+                    } catch (\Error $error) {
+                        Log::error('AsyncLogger::scheduleCallHandler - Unserialize error. Message: ' . $error->getMessage());
+                    }
+                }
+            }
+            DB::connection('logsdb')->collection('access')->insert($arr);
+            Log::info('AsyncLogger::scheduleCallHandler - Logs saved successful. Saved records volume: ' . count($arr));
+        }
+    }
+
     /**
      * @param Application $app
      */
     static function registerAsyncLogger($app) {
         $app->booted(function () use ($app)  {
             $schedule = $app->make(Schedule::class);
-            $schedule->call(function () {
-                Log::info('AsyncLogger::registerAsyncLogger - Check logs for saving');
-                if (file_exists(storage_path(AsyncLogger::$FILE))) {
-                    Log::info('AsyncLogger::registerAsyncLogger - Logs found. Saving logs to database');
-                    /** @var string $content */
-                    $content = file_get_contents(storage_path(AsyncLogger::$FILE));
-                    unlink(storage_path(AsyncLogger::$FILE));
-                    /** @var array $requests */
-                    $requests = explode("\n", $content);
-                    $arr = [];
-                    foreach ($requests as $request) {
-                        if (strlen($request) != 0) {
-                            $record = unserialize($request);
-                            $arr[] = $record;
-                        }
-                    }
-                    DB::connection('logsdb')->collection('access')->insert($arr);
-                    Log::info('AsyncLogger::registerAsyncLogger - Logs saved successful. Saved records volume: ' . count($arr));
-                }
-            })->everyMinute();
+            $schedule
+                ->call(function () { AsyncLogger::scheduleCallHandler(); })
+                ->everyMinute();
         });
     }
 
@@ -44,6 +51,10 @@ class AsyncLogger
      * @param mixed $request
      */
     static function logRequest($request) {
-        file_put_contents(storage_path(AsyncLogger::$FILE), serialize($request) . "\n", FILE_APPEND);
+        try {
+            file_put_contents(storage_path(AsyncLogger::$FILE), serialize($request) . "\n", FILE_APPEND);
+        } catch (\Error $error) {
+            Log::error('AsyncLogger::logRequest - Write file error. Message: ' . $error->getMessage());
+        }
     }
 }
